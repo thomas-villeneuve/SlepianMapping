0acbb1b0f18f1b88e2f638dc7d87b6da52a5cdbf
thomas-villeneuve <81256885+thomas-villeneuve@users.noreply.github.com>
2021-05-11 16:48:11 -0400

    Rewritten consistency test
    Changed to fit for the slepian coefficients rather than the slepian light curves.

--- consistency test.py
+++ consistency test.py
@@ -1,161 +1,228 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[43]:
 
 
-import starry
+from starry.kepler import Primary, Secondary, System
 import numpy as np
+import slepian
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 import emcee
 import corner
 from IPython.display import display, Math
-
-
-# In[2]:
-
-
-# Compute lightcurves of order one Slepian functions
-time = np.linspace(-0.1, 0.1, 1000)
-star = starry.kepler.Primary()
-s1 = starry.kepler.Secondary(lmax = 1)
-s1[1,0] = np.sqrt(2)/2
-s1.porb = 1
-s1.prot = 1
-s1.a = 5
-s1.tref = 0.5
-s1.inc = 87
-s1.L = 0.01
-systems1 = starry.kepler.System(star, s1)
-systems1.compute(time)
-F1 = s1.lightcurve
-
-s2 = starry.kepler.Secondary(lmax = 1)
-s2[1,-1] = 1
-s2.prot = 1
-s2.a = 5
-s2.tref = 0.5
-s2.inc = 87
-s2.L = 0.01
-systems2 = starry.kepler.System(star, s2)
-systems2.compute(time)
-F2 = s2.lightcurve
-
-s3 = starry.kepler.Secondary(lmax = 1)
-s3[1,1] = 1
-s3.prot = 1
-s3.a = 5
-s3.tref = 0.5
-s3.inc = 87
-s3.L = 0.01
-systems3 = starry.kepler.System(star, s3)
-systems3.compute(time)
-F3 = s3.lightcurve
-
-s4 = starry.kepler.Secondary(lmax = 1)
-s4[1,-1] = -np.sqrt(2)/2
-s4.prot = 1
-s4.a = 5
-s4.tref = 0.5
-s4.inc = 87
-s4.L = 0.01
-systems4 = starry.kepler.System(star, s4)
-systems4.compute(time)
-F4 = s4.lightcurve
-
-
-# In[7]:
-
-
 np.random.seed(264)
 
-# Create an arbitray star-planet system
-planet1 = starry.kepler.Secondary(lmax = 1)
-planet1[1,0] = 0.7 *np.sqrt(0.5)
-planet1[1,-1] = 0.3   
-planet1.porb = 1
-planet1.prot = 1
-planet1.a = 5
-planet1.tref = 0.5
-planet1.inc = 87
-planet1.L = 0.01
-planet1.show()
 
-# Compute the planet lightcurve
-system1 = starry.kepler.System(star, planet1)
-system1.compute(time)
-flux = planet1.lightcurve
-
-# Generate some synthetic data
-yerr = 0.001
-flux += yerr * np.random.randn(1000)
+# In[44]:
 
 
-# In[17]:
+def changeOfBasis(L):
+    """Change of basis matrix."""
+    return np.array(slepian.slepianToHarmonic(L),dtype=float)
 
 
-# Plot the lightcurve and synthetic data
-plt.figure(figsize=(8,8))
-plt.plot(time,0.7*F1 + 0.3*F2, 'r-',label = 'lightcurve')
-plt.plot(time,flux,'mo', markersize=2,label = 'data with noise')
-plt.xlabel("Time [Days]", fontsize = 16)
-plt.ylabel('Planet Flux', fontsize = 16)
-plt.legend()
+# In[45]:
 
 
-# In[8]:
+def setCoeffs(X,r,planet):
+    """Takes a harmonic vector X and sets the starry map parameters."""
+    
+    # Pull out the Y0,0 coefficient X0 to set L
+    X0 = X[0] 
+    
+    # Renormalize so that X0 = 1
+    X = X/X0 
+    
+    # Set luminosity
+    planet.L = r*X0 
+    
+    # Set map coefficients
+    planet[1,:] = X[1:] 
+    
+    return
 
 
-# Define the likelihood function
-def log_likelihood(theta, time, flux):
-    a,b,c,d,s = theta
-    model = a*F1 + b*F2 +c*F3 + d*F4
-    sigma2 = s ** 2 
-    return -0.5 * np.sum((flux - model) ** 2 / sigma2 + np.log(sigma2))
-
-# Find the numerical optimum of the likelihood function
-nll = lambda *args: -log_likelihood(*args)
-initial = np.array([0.1,0.1,0.1,0.1,0.1])
-soln = minimize(nll, initial, args = (time, flux))
-a_ml, b_ml, c_ml, d_ml, s_ml = soln.x
-
-print("a = {0:.3f}".format(a_ml))
-print("b = {0:.3f}".format(b_ml))
-print("c = {0:.3f}".format(c_ml))
-print("d = {0:.3f}".format(d_ml))
-print("s = {0:.3f}".format(s_ml))
+# In[46]:
 
 
-# In[9]:
+def getLightcurve(X,params,time,star,planet,L):
+    """Creates a planet-star system and generate the planet lightcurve."""
+    
+    # Extract parameters (more to be added later)
+    r = params
+    
+    # Set map coefficients
+    setCoeffs(X,r,planet)
+    
+    # Instantiate the system
+    system = System(star, planet)
+    
+    # Compute the flux
+    system.compute(time)
+    flux = planet.lightcurve
+    
+    return flux
 
 
-# Define the full log posterior probability function
-def log_prior(theta):
-    a,b,c,d,s = theta
-    if -1 < a < 1 and -1 < b < 1 and -1 < c < 1 and -1 < d < 1 and 0 < s < 1:
-        return 0.0
-    return -np.inf
+# In[47]:
 
-def log_probability(theta, time, flux):
-    lp = log_prior(theta)
-    if not np.isfinite(lp):
+
+def lnPrior(X):
+    """Log prior probability."""
+    
+    # Map coefficents should be |X| < 1 and 
+    # X0 > 0 so that luminosity is positive. 
+    if np.any(X < -1) or np.any(X > 1) or (X[0] <= 0): 
         return -np.inf
-    return lp + log_likelihood(theta, time, flux)
+    else:
+        return 0
 
 
-# In[10]:
+# In[48]:
 
 
-# Run 5,000 steps of MCMC
-import emcee
-pos = soln.x + 1e-4 * np.random.randn(32, 5)
-nwalkers, ndim = pos.shape
-sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(time, flux))
-sampler.run_mcmc(pos, 5000, progress=True)
+def lnLike(T,params,time,flux,flux_err,star,planet,L,C):
+    """Log likelihood."""
+    
+    # Convert from slepian to harmonic vector.
+    X = np.dot(C,T) 
+    
+    # Check that X fits the priors.
+    l = lnPrior(X)
+    if np.isinf(l):
+        return l
+    
+    # Create a system and get the computed flux.
+    c_flux = getLightcurve(X,params,time,star,planet,L)
+    
+    # Caluculate log likelihood.
+    like = np.sum(((flux - c_flux)/flux_err)**2 + np.log(2*np.pi*flux_err**2))
+    l += -0.5*like
+    
+    return l
 
 
-# In[11]:
+# In[49]:
+
+
+def maxLike(lnlike,guess,params,time,flux,flux_err,star,planet,L,C):
+    """Maximum likelihood solution for slepian coefficients."""
+    
+    # Define negative log likelihood
+    nlnLike = lambda *args: -lnLike(*args)
+
+    # Find the numerical optimum of the likelihood function
+    soln = minimize(nlnLike, guess, args = (params,time,flux,flux_err,star,planet,L,C),method='Nelder-Mead')
+    T_maxl = soln.x
+
+    return T_maxl
+
+
+# In[50]:
+
+
+#Instantiate a star
+star = Primary()
+
+# Instantiate the planet
+# We'll use the same planet in MCMC but change the params each time as necessary 
+planet = Secondary(L)
+planet.a = 5 # Orbital semi-major axis in units of the stellar radii.
+planet.inc = 87 # Orbital inclination (90 degrees is an edge-on orbit)
+planet.porb = 1 # Orbital period in Julian days
+planet.prot = 1 # Rotation period in days (synchronous if prot=porb)
+#planet.ecc = 0 # Eccentricity
+#planet.w = 0  # Longitude of pericenter in degrees (meaningless for ecc=0)
+planet.tref = 0.5 # Time of transit in Julian days
+
+# Generate the lightcurve for true planet 
+S1 = 0.7
+S2 = 0.3
+S3 = 0.0
+S4 = 0.0
+
+T_true = np.array([S1,S2,S3,S4])
+X_true = np.dot(C,T_true)
+
+params = 0.01
+
+flux_true = getLightcurve(X_true,params,time,star,planet,L)
+
+# Add noise
+flux_err = 0.001
+flux = flux_true + flux_err*np.random.randn(1000)
+
+
+# In[51]:
+
+
+# Define a guess
+guess = np.array([0.1,0.1,0.1,0.1])
+
+# Find the max likelihood solution
+T_maxl = max_likelihood(lnlike,guess,params,time,flux,flux_err,star,planet,L,C)
+
+# Print results
+print("Maximum likelihood estimates:")
+print("T_1 = {0:.3f}".format(T_maxl[0]))
+print("T_2 = {0:.3f}".format(T_maxl[1]))
+print("T_3 = {0:.3f}".format(T_maxl[2]))
+print("T_4 = {0:.3f}".format(T_maxl[3]))
+
+
+# Plot max likihood flux
+X_maxl = np.dot(C,T_maxl)
+flux_maxl = getLightcurve(X_maxl,params,time,star,planet,L)
+
+plt.figure(figsize=(10,10))
+plt.plot(time,flux, color = '#FCCC25',marker = '.',linestyle ='None',label = 'Flux with noise')
+plt.plot(time,flux_maxl, color = '#E06461', marker = 'None', label = 'Max likelihood flux')
+plt.plot(time,flux_true, color = '#6A00A7', marker = 'None', label = 'True Flux')
+plt.legend(fontsize=10)
+plt.xlabel("Time in Days",fontsize=14)
+plt.ylabel("Thermal Flux",fontsize=14)
+plt.plot()
+
+
+# In[35]:
+
+
+L = 1
+C = changeOfBasis(L)
+time = np.linspace(-0.1, 0.1, 1000)
+
+
+# In[36]:
+
+
+# Initialize around the max likelihood solution
+T_pos = T_maxl + 1e-4 * np.random.randn(32,4)
+nwalkers,ndim = T_pos.shape
+
+# Run an MCMC chain
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnlike, args = (params,time,flux,flux_err,star,planet,L,C))
+sampler.run_mcmc(T_pos, 5000, progress=True)                                
+
+
+# In[52]:
+
+
+fig, axes = plt.subplots(4, figsize=(10, 7), sharex=True)
+samples = sampler.get_chain()
+labels = [r"$T_1$", r"$T_2$", r"$T_3$",r"$T_4$"]
+for i in range(ndim):
+    ax = axes[i]
+    ax.plot(samples[:, :, i], alpha=0.3,lw = 1)
+    ax.set_xlim(0, len(samples))
+    ax.set_ylabel(labels[i])
+    ax.yaxis.set_label_coords(-0.1, 0.5)
+
+axes[-1].set_xlabel("Number of Steps");
+
+
+# In[53]:
 
 
 # An estimate of the integrated autocorrelation time
@@ -163,25 +230,17 @@ tau = sampler.get_autocorr_time()
 print(tau)
 
 
-# In[12]:
+# In[54]:
 
 
-labels = ["a", "b", "c", 'd','s']
 flat_samples = sampler.get_chain(discard=100, thin=30, flat=True)
 print(flat_samples.shape)
 
 
-# In[13]:
+# In[57]:
 
 
-# Corner plot
-fig = corner.corner(flat_samples, labels=labels, truths=[0.7, 0.3,0,0,0.001])
-
-
-# In[14]:
-
-
-# Display the results:
+labels = ["T_1","T_2","T_3","T_4"]
 for i in range(ndim):
     mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
     q = np.diff(mcmc)
@@ -190,15 +249,31 @@ for i in range(ndim):
     display(Math(txt))
 
 
-# In[19]:
+# In[58]:
 
 
-# Plot the true lightcurve, synthetic data and the recovered lightcurve
-plt.figure(figsize=(8,8))
-plt.plot(time,flux,'mo', markersize=2, label = 'Data with Noise')
-plt.plot(time,0.7*F1 + 0.3*F2, 'r--',label = 'True lightcurve')
-plt.plot(time, a_ml * F1 + b_ml*F2 +c_ml*F3 + d_ml*F4, 'g-', label = 'Recovered lightcurve')
-plt.xlabel("Time [Days]", fontsize = 16)
-plt.ylabel('Planet Flux', fontsize = 16)
-plt.legend()
+T_r = np.array([0.703,0.339,-0.001,-0.184])
+X_r = np.dot(C,T_r)
+
+flux_r = getLightcurve(X_r,params,time,star,planet,L)
+
+
+# In[59]:
+
+
+plt.figure(figsize=(10,10))
+plt.plot(time,flux, color = '#FCCC25',marker = '.',linestyle ='None',label = 'Flux with noise')
+plt.plot(time,flux_maxl, color = '#E06461', marker = 'None', label = 'Max likelihood flux')
+plt.plot(time,flux_r, color = '#B02A8F', marker = 'None', label = 'MCMC result')
+plt.plot(time,flux_true, color = '#6A00A7', marker = 'None', label = 'True Flux')
+plt.legend(fontsize=10)
+plt.xlabel("Time in Days",fontsize=14)
+plt.ylabel("Thermal Flux",fontsize=14)
+plt.plot()
+
+
+# In[ ]:
+
+
+
 
